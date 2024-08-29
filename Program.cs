9de96cad8305ecc32382ad6e7111d405438926d9
Wilson Santos <contato@wilsonsantos.net>
2024-08-29 15:34:44 -0300

    Faz as configurações para utilizar o serilog e o loki

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Prometheus;
 using Prometheus.Client.DependencyInjection;
+using Serilog;
+using Serilog.Sinks.Grafana.Loki;
 using WS.WorkerExample.Application.Interfaces;
 using WS.WorkerExample.Application.Services;
 using WS.WorkerExample.Data;
@@ -11,30 +13,44 @@ namespace WS.WorkerExample
     {
         public static void Main(string[] args)
         {
-            var builder = Host.CreateApplicationBuilder(args);
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.GrafanaLoki("http://loki:3100", new []
+                {
+                    new LokiLabel
+                    {
+                        Key = "app",
+                        Value = "workerExample"
+                    }
+                })
+                .CreateLogger();
 
-            builder.Logging.ClearProviders();
-            builder.Logging.AddConsole();
-            builder.Logging.AddDebug();
-
-            builder.Services
-                .AddHostedService<Worker>()
-                .AddSingleton<IHttpService, HttpService>()
-                .AddSingleton<IMetricFactory>(Metrics.DefaultFactory)
-                .AddScoped<IWorkerService, WorkerService>();
-
-            var connectionString = builder.Configuration.GetConnectionString("default");
-
-            builder.Services.AddDbContext<AppDbContext>(options =>
+            try
             {
-                options.UseSqlServer(connectionString);
-            });
-            
-            var host = builder.Build();
+                var builder = Host.CreateDefaultBuilder(args)
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        services
+                            .AddHostedService<Worker>()
+                            .AddSingleton<IHttpService, HttpService>()
+                            .AddSingleton<IMetricFactory>(Metrics.DefaultFactory)
+                            .AddScoped<IWorkerService, WorkerService>();
 
-            var metricServer = new KestrelMetricServer(port:8080);
-            metricServer.Start();
-            host.Run();
+                        var connectionString = hostContext.Configuration.GetConnectionString("default");
+                        services.AddDbContext<AppDbContext>(options => { options.UseSqlServer(connectionString); });
+                    })
+                    .UseSerilog(); 
+                var host = builder.Build();
+
+                var metricServer = new KestrelMetricServer(port: 8080);
+                metricServer.Start();
+                host.Run();
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,123 @@
+﻿# Worker Example
+Projeto para fazer testes com um worker.
+
+## itens chaves da app
+Foi configurado o Serilog para enviar os logs diretamente para o loki, a url está hardcoded até o momento.
+
+
+## Deploy
+o docker-compose.yml está configurado pra subir a aplicação especificando uma imagem e a versão, o banco de dados, 
+grafana e loki.
+
+
+## Configuração do Grafana
+Exemplo de uma dashboard para consultar os logs.
+
+```json
+
+{
+  "annotations": {
+    "list": [
+      {
+        "builtIn": 1,
+        "datasource": {
+          "type": "grafana",
+          "uid": "-- Grafana --"
+        },
+        "enable": true,
+        "hide": true,
+        "iconColor": "rgba(0, 211, 255, 1)",
+        "name": "Annotations & Alerts",
+        "type": "dashboard"
+      }
+    ]
+  },
+  "editable": true,
+  "fiscalYearStartMonth": 0,
+  "graphTooltip": 0,
+  "id": 6,
+  "links": [],
+  "panels": [
+    {
+      "datasource": {
+        "default": false,
+        "type": "loki",
+        "uid": "adw7py7jsi4n4c"
+      },
+      "gridPos": {
+        "h": 17,
+        "w": 24,
+        "x": 0,
+        "y": 0
+      },
+      "id": 1,
+      "options": {
+        "dedupStrategy": "none",
+        "enableLogDetails": true,
+        "prettifyLogMessage": true,
+        "showCommonLabels": false,
+        "showLabels": false,
+        "showTime": true,
+        "sortOrder": "Descending",
+        "wrapLogMessage": true
+      },
+      "targets": [
+        {
+          "datasource": {
+            "type": "loki",
+            "uid": "adw7py7jsi4n4c"
+          },
+          "editorMode": "code",
+          "expr": "{app=\"workerExample\"} |= \"$search\" ",
+          "queryType": "range",
+          "refId": "A"
+        }
+      ],
+      "title": "Logs do Serilog",
+      "type": "logs"
+    }
+  ],
+  "schemaVersion": 39,
+  "tags": [],
+  "templating": {
+    "list": [
+      {
+        "current": {
+          "isNone": true,
+          "selected": false,
+          "text": "None",
+          "value": ""
+        },
+        "datasource": {
+          "type": "prometheus",
+          "uid": "fdw6t3gkogq2oa"
+        },
+        "definition": "",
+        "hide": 0,
+        "includeAll": false,
+        "label": "Busca logs",
+        "multi": false,
+        "name": "search",
+        "options": [],
+        "query": "",
+        "refresh": 1,
+        "regex": "",
+        "skipUrlSync": false,
+        "sort": 0,
+        "type": "query"
+      }
+    ]
+  },
+  "time": {
+    "from": "now-6h",
+    "to": "now"
+  },
+  "timepicker": {},
+  "timezone": "America/Sao_Paulo",
+  "title": "Logs Loki",
+  "uid": "adwb8xsdm7o5cd",
+  "version": 3,
+  "weekStart": ""
+}
+
+```
